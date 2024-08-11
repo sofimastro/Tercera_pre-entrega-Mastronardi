6484c8c14a402e84bf4116fd012d6ab5fba23530
Sofia Mastronardi <sofiammastronardi@gmail.com>
2024-08-11 12:21:26 -0300

    otros cambios

--- AppCoder/tests.py
+++ AppCoder/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
+from .models import Curso, Profesor, Estudiante, Entregable
 
-# Create your tests here.
+class CursoModelTest(TestCase):
+    def setUp(self):
+        Curso.objects.create(nombre="Curso 1", comision=1234)
+
+    def test_curso_str(self):
+        curso = Curso.objects.get(nombre="Curso 1")
+        self.assertEqual(str(curso), "Curso 1 - 1234")
--- AppCoder/views.py
+++ AppCoder/views.py
@@ -1,26 +1,29 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from .models import Curso, Estudiante, Profesor, Entregable
+from django.shortcuts import render, redirect
+from .models import Curso, Profesor, Estudiante, Entregable
+from .forms import CursoForm
 
-def inicio(request):
-    return HttpResponse("Vista inicio")
-
-def cursos(request):
-    return HttpResponse("Vista cursos")
-
-def profesores(request):
-    return HttpResponse("Vista profesores")
-
-def estudiantes(request):
-    return HttpResponse("Vista estudiantes")
-
-def entregables(request):
-    return HttpResponse("Vista entregables")
+def index(request):
+    return render(request, 'AppCoder/index.html')
 
 def curso_formulario(request):
     if request.method == 'POST':
-        curso = Curso(nombre=request.POST['curso'], camada=request.POST['camada'])
-        curso.save()
-        return render(request, "AppCoder/index.html")
-    return render(request, "AppCoder/curso_formulario.html")
+        form = CursoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+    else:
+        form = CursoForm()
+    return render(request, 'AppCoder/curso_formulario.html', {'form': form})
+
+def profesores_list(request):
+    profesores = Profesor.objects.all()
+    return render(request, 'AppCoder/profesores_list.html', {'profesores': profesores})
+
+def estudiantes_list(request):
+    estudiantes = Estudiante.objects.all()
+    return render(request, 'AppCoder/estudiantes_list.html', {'estudiantes': estudiantes})
+
+def entregables_list(request):
+    entregables = Entregable.objects.all()
+    return render(request, 'AppCoder/entregables_list.html', {'entregables': entregables})
 
