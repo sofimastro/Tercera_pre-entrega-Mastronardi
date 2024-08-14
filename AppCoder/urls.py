df0554556491a46160519d7e934df9cb49b82d67
Sofia Mastronardi <sofiammastronardi@gmail.com>
2024-08-14 20:02:55 -0300

    formularios para todos los modelos

--- AppCoder/urls.py
+++ AppCoder/urls.py
@@ -1,12 +1,15 @@
-# AppCoder/urls.py
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('curso_formulario/', views.curso_formulario, name='curso_formulario'),
+    path('profesor_formulario/', views.crear_profesor, name='profesor_formulario'),
+    path('estudiante_formulario/', views.crear_estudiante, name='estudiante_formulario'),
+    path('entregable_formulario/', views.crear_entregable, name='entregable_formulario'),
     path('profesores/', views.profesores_list, name='profesores_list'),
     path('estudiantes/', views.estudiantes_list, name='estudiantes_list'),
     path('entregables/', views.entregables_list, name='entregables_list'),
     path('buscar/', views.buscar, name='buscar'),
 ]
+
--- AppCoder/views.py
+++ AppCoder/views.py
@@ -2,57 +2,69 @@ from django.shortcuts import render, redirect
 from .forms import CursoForm, EstudianteForm, ProfesorForm, EntregableForm, SearchForm
 from .models import Curso, Estudiante, Profesor, Entregable
 
-# Vista para crear objetos
-def crear_curso(request):
-    if request.method == 'POST':
-        form = CursoForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('listado_cursos')  # Redirige a una página de listado de cursos
-    else:
-        form = CursoForm()
-    return render(request, 'AppCoder/crear_curso.html', {'form': form})
+# Vista para la página de inicio
+def index(request):
+    return render(request, 'AppCoder/index.html')
 
-def crear_estudiante(request):
-    if request.method == 'POST':
-        form = EstudianteForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('listado_estudiantes')  # Redirige a una página de listado de estudiantes
-    else:
-        form = EstudianteForm()
-    return render(request, 'AppCoder/crear_estudiante.html', {'form': form})
-
-def crear_profesor(request):
-    if request.method == 'POST':
-        form = ProfesorForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('listado_profesores')  # Redirige a una página de listado de profesores
-    else:
-        form = ProfesorForm()
-    return render(request, 'AppCoder/crear_profesor.html', {'form': form})
-
-def crear_entregable(request):
-    if request.method == 'POST':
-        form = EntregableForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('listado_entregables')  # Redirige a una página de listado de entregables
-    else:
-        form = EntregableForm()
-    return render(request, 'AppCoder/crear_entregable.html', {'form': form})
-
+# Vista para crear curso
 def curso_formulario(request):
     if request.method == 'POST':
         form = CursoForm(request.POST)
         if form.is_valid():
             form.save()
-            # Redirigir a alguna página de éxito
+            # Redirigir a alguna página de éxito (lista de cursos, por ejemplo)
+            return redirect('curso_formulario')
     else:
         form = CursoForm()
-    
     return render(request, 'AppCoder/curso_formulario.html', {'form': form})
+
+# Vista para crear estudiante
+def crear_estudiante(request):
+    if request.method == 'POST':
+        form = EstudianteForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('estudiantes_list')  # Redirige a la lista de estudiantes
+    else:
+        form = EstudianteForm()
+    return render(request, 'AppCoder/estudiante_formulario.html', {'form': form})
+
+# Vista para crear profesor
+def crear_profesor(request):
+    if request.method == 'POST':
+        form = ProfesorForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('profesores_list')  # Redirige a la lista de profesores
+    else:
+        form = ProfesorForm()
+    return render(request, 'AppCoder/profesor_formulario.html', {'form': form})
+
+# Vista para crear entregable
+def crear_entregable(request):
+    if request.method == 'POST':
+        form = EntregableForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('entregables_list')  # Redirige a la lista de entregables
+    else:
+        form = EntregableForm()
+    return render(request, 'AppCoder/entregable_formulario.html', {'form': form})
+
+# Vista para listar profesores
+def profesores_list(request):
+    profesores = Profesor.objects.all()
+    return render(request, 'AppCoder/profesores_list.html', {'profesores': profesores})
+
+# Vista para listar estudiantes
+def estudiantes_list(request):
+    estudiantes = Estudiante.objects.all()
+    return render(request, 'AppCoder/estudiantes_list.html', {'estudiantes': estudiantes})
+
+# Vista para listar entregables
+def entregables_list(request):
+    entregables = Entregable.objects.all()
+    return render(request, 'AppCoder/entregables_list.html', {'entregables': entregables})
 
 # Vista para buscar objetos
 def buscar(request):
@@ -72,21 +84,3 @@ def buscar(request):
             'entregables': Entregable.objects.filter(nombre__icontains=query)
         }
     return render(request, 'AppCoder/buscar.html', {'form': form, 'results': results})
-
-def index(request):
-    return render(request, 'AppCoder/index.html')
-
-# Vista para listar profesores
-def profesores_list(request):
-    profesores = Profesor.objects.all()
-    return render(request, 'AppCoder/profesores_list.html', {'profesores': profesores})
-
-# Vista para listar estudiantes
-def estudiantes_list(request):
-    estudiantes = Estudiante.objects.all()
-    return render(request, 'AppCoder/estudiantes_list.html', {'estudiantes': estudiantes})
-
-# Vista para listar entregables
-def entregables_list(request):
-    entregables = Entregable.objects.all()
-    return render(request, 'AppCoder/entregables_list.html', {'entregables': entregables})
